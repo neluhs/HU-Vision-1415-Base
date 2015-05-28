33a4a87f7bfab4ce65d4c1e0b0d324dd53439f7d
neluhs <stefan.dijkman@student.hu.nl>
2015-05-28 14:38:58 +0200

    ging iets fout, weer terug op oude versie

--- source/ExternalDLL/ExternalDLL/RGBImageStudent.cpp
+++ source/ExternalDLL/ExternalDLL/RGBImageStudent.cpp
@@ -5,37 +5,20 @@ RGBImageStudent::RGBImageStudent() : RGBImage() {
 }
 
 RGBImageStudent::RGBImageStudent(const RGBImageStudent &other) : RGBImage(other.getWidth(), other.getHeight()) {
-	pixelStorage = other.pixelStorage;
+	int size = (other.getWidth() * other.getHeight());
+	pixelStorage = new RGB[size];
+	for (int i = 0; i < size; ++i) {
+		pixelStorage[i] = other.getPixel(i);
+	}
 }
 
 RGBImageStudent::RGBImageStudent(const int width, const int height) : RGBImage(width, height) {
-	long size = width*height;
-	pixelStorage = new RGB[size];
-	for (int i = 0; i < size; ++i) {
-		pixelStorage[i] = { {0} };
-	}
-	imageWidth = width;
-	imageHeight = height;
+	pixelStorage = new RGB[width*height];
 }
 
 RGBImageStudent::~RGBImageStudent() {
 	delete[] pixelStorage;
 }
-
-/* OLD SHIZZLE
-void RGBImageStudent::set(const int width, const int height) {
-	RGBImage::set(width, height);
-	int size = width*height;
-	RGB* pixelStorageTemp = new RGB[size];
-	for (int i = 0; i < size; ++i) {
-		pixelStorageTemp[i] = pixelStorage[i];
-	}
-	delete[] pixelStorage;
-	pixelStorage = pixelStorageTemp;
-	imageWidth = width;
-	imageHeight = height;
-}
-*/
 
 void RGBImageStudent::set(const int width, const int height) {
 	RGB* newImage = new RGB[width*height];
@@ -44,15 +27,12 @@ void RGBImageStudent::set(const int width, const int height) {
 		newImage[i] = (oldSize ? RGB{} : pixelStorage[i]);
 	}
 	RGBImage::set(width, height);
-	RGB * tmp = pixelStorage;
-	delete[] tmp;
+
 	pixelStorage = newImage;
 }
 
 void RGBImageStudent::set(const RGBImageStudent &other) {
 	RGBImage::set(other.getWidth(), other.getHeight());
-	RGB * tmp = pixelStorage;
-	delete[] tmp;
 	pixelStorage = new RGB[other.getWidth() * other.getHeight()];
 	int maxSize = other.getWidth() * other.getHeight();
 	for (int i = 0; i < maxSize; i++){
@@ -60,19 +40,8 @@ void RGBImageStudent::set(const RGBImageStudent &other) {
 	}
 }
 
-
-void RGBImageStudent::set(const RGBImageStudent &other) {
-	RGBImage::set(other.getWidth(), other.getHeight());
-	int size = other.getWidth() * other.getHeight();
-	for (int i = 0; i < size; i++) {
-		pixelStorage[i] = other.pixelStorage[i];
-	}
-	imageWidth = other.getWidth();
-	imageHeight = other.getHeight();
-}
-
 void RGBImageStudent::setPixel(int x, int y, RGB pixel) {
-	pixelStorage[(y*RGBImage::getWidth()) + x] = pixel;
+	pixelStorage[x + (y*RGBImage::getWidth())] = pixel;
 }
 
 void RGBImageStudent::setPixel(int i, RGB pixel) {
@@ -80,7 +49,7 @@ void RGBImageStudent::setPixel(int i, RGB pixel) {
 }
 
 RGB RGBImageStudent::getPixel(int x, int y) const {
-	return pixelStorage[(y*RGBImage::getWidth()) + x];
+	return pixelStorage[x + (y*RGBImage::getWidth())];
 }
 
 RGB RGBImageStudent::getPixel(int i) const {
